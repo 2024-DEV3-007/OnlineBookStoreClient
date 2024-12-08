198ddfdf7eabf178f20a53a557fb17d2fe81788b
reshm <reshmapsuresh20@gmail.com>
2024-12-09 03:01:23 +0530

    feat : User select the item quantity and add to the cart

--- onlineBookStoreClient/src/container/bookStore/index.jsx
+++ onlineBookStoreClient/src/container/bookStore/index.jsx
@@ -9,6 +9,10 @@ import "./index.css";
   const { username, password } = location.state || {};
   const [books, setBooks] = useState([]);
   const [error, setError] = useState(null);
+  const [quantities, setQuantities] = useState({});
+  const [cartItem, setCart] = useState([]);
+  const [addedBooks, setAddedBooks] = useState(new Set());
+  const [order, setOrder] = useState(false);
 
   const credentials = `${username}:${password}`;
   const encodedCredentials = btoa(credentials);
@@ -33,6 +37,41 @@ const fetchBooks = () => {
       });
   };
 
+const handleQuantityChange = (bookId, value) => {
+    setQuantities((prev) => ({ ...prev, [bookId]: parseInt(value, 10) }));
+  };
+  const handleAddToCart = (book) => {
+      const cartItemsToAdd = { bookId: book.id, quantity: quantities[book.id] || 1 };
+      const updatedCart = [...cartItem, cartItemsToAdd]
+      setCart(updatedCart);
+      const addedBooksSet = new Set(updatedCart.map(item => item.bookId));
+      setAddedBooks(addedBooksSet);
+
+      const requestBody = {
+        items: updatedCart,
+        ordered: order,
+      };
+      console.log("requestBody:", requestBody);
+      axios
+        .post("http://localhost:8080/api/cart/updateCart", requestBody, {
+          headers: {
+            Authorization: `Basic ${encodedCredentials}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          setAddedBooks((prev) => new Set(prev).add(book.id));
+        })
+        .catch((error) => {
+          console.error("Error adding to cart:", error);
+          alert("Failed to add book to cart.");
+        });
+    };
+
+const getTotalItems = () => {
+  return cartItem.reduce((total, item) => total + item.quantity, 0);
+};
+
   return (
     <div className="bookscreen-container">
       <div className="header">
@@ -41,14 +80,28 @@ const fetchBooks = () => {
       </div>
       <div className="user-greeting-container">
         <span className="username" data-testid="user-greeting">Hi,{username}</span>
-              <button className="cart-btn" data-testid="cart-button"> Cart  </button>
+          <button className="cart-btn" data-testid="cart-button">
+              Cart {getTotalItems() > 0 && <span>({getTotalItems()})</span>}
+          </button>
       </div>
       <div className="book-list">
               {books.map((book) => (
                 <div key={book.id} className="book-item" data-testid="book-item">
-                  <h3>{book.title}</h3>
+                  <h3 data-testid="book-title">{book.title}</h3>
                   <p>Author: {book.author}</p>
                   <p>Price: €{book.price}</p>
+                  <div className="book-actions">
+                  <select className="quantity-picker" data-testid={`quantity-picker-${book.id}`}
+                        value={quantities[book.id] || 1}
+                        onChange={(e) => handleQuantityChange(book.id, e.target.value)}>
+                        <option value="" disabled> </option>
+                        {[...Array(50).keys()].map((i) => (
+                            <option key={i + 1} value={i + 1}> {i + 1} </option>
+                        ))}
+                  </select>
+                  <button onClick={() => handleAddToCart(book)} className="add-to-cart-btn" data-testid={`add-to-cart-${book.id}`}>
+                        Add to Cart</button>
+                   </div>
                 </div>
               ))}
             </div>
--- onlineBookStoreClient/src/container/bookStore/index.test.jsx
+++ onlineBookStoreClient/src/container/bookStore/index.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
-import { render, screen, fireEvent} from "@testing-library/react";
+import { render, screen, fireEvent, waitFor} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import BookStore from "./index";
 
@@ -45,3 +45,99 @@ test('Displays Online BookStore Heading , Cart in the screen', () => {
     const logoutButton = screen.getByTestId('logout-button');
     expect(logoutButton).toBeInTheDocument();
 })
+
+test('Quantity Picker is displayed and user can select the quantity', async() => {
+
+    const books = [
+        { id: 1, title: 'Book 1', author: 'Author 1', price: 10 },
+        { id: 2, title: 'Book 2', author: 'Author 2', price: 15 },
+    ];
+
+    axios.get.mockResolvedValueOnce({ data: books });
+    render(<Router initialEntries={["/books", { state: { username: "username", password: "abc" } }]}>
+            <BookStore/></Router>
+    );
+
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/api/books', {
+          headers: { Authorization: `Basic dW5kZWZpbmVkOnVuZGVmaW5lZA==` },
+    });
+        await waitFor(() => {
+              books.forEach((book) => {
+                expect(screen.getByText(book.title)).toBeInTheDocument();
+                expect(screen.getByText(`Author: ${book.author}`)).toBeInTheDocument();
+                expect(screen.getByText(`Price: €${book.price}`)).toBeInTheDocument();
+              });
+        });
+
+    const select = screen.getByTestId('quantity-picker-1');
+    fireEvent.change(select, { target: { value: '3' } });
+    expect(select.value).toBe('3');
+})
+
+test("User Select the quantity and add item to the cart",  async() => {
+
+    const books = [
+        { id: 1, title: 'Book 1', author: 'Author 1', price: 10 },
+        { id: 2, title: 'Book 2', author: 'Author 2', price: 15 },
+    ];
+
+    axios.get.mockResolvedValueOnce({ data: books });
+    axios.post.mockResolvedValue({data: {success: true,message: "Cart updated successfully",
+        updatedCart: [{ bookId: 1, quantity: 2 },{ bookId: 3, quantity: 1 },]}
+    });
+
+    render(<Router initialEntries={["/books", { state: { username: "username", password: "abc" } }]}>
+            <BookStore/></Router>);
+
+    await waitFor(() => {
+            books.forEach((book) => {expect(screen.getByText(book.title)).toBeInTheDocument();});
+    });
+
+    const select = screen.getByTestId('quantity-picker-1');
+    fireEvent.change(select, { target: { value: '3' } });
+    fireEvent.click(screen.getByTestId('add-to-cart-1'));
+
+    await waitFor(() => {
+        books.forEach((book) => {expect(select.value).toBe('3');});
+        });
+
+    expect(axios.post).toHaveBeenCalledWith('http://localhost:8080/api/cart/updateCart',{
+                  items: [{ bookId: 1, quantity: 3 }],
+                  ordered: false,},{
+                    headers: { Authorization: `Basic dW5kZWZpbmVkOnVuZGVmaW5lZA==` },
+                    }
+    );
+  });
+
+test("The count of added items should be displayed in the cart",  async() => {
+
+    const books = [
+        { id: 1, title: 'Book 1', author: 'Author 1', price: 10 },
+        { id: 2, title: 'Book 2', author: 'Author 2', price: 15 },
+    ];
+
+    axios.get.mockResolvedValueOnce({ data: books });
+    axios.post.mockResolvedValue({data: {success: true,message: "Cart updated successfully",
+        updatedCart: [{ bookId: 1, quantity: 2 },{ bookId: 3, quantity: 1 },]}
+    });
+
+    render(<Router initialEntries={["/books", { state: { username: "username", password: "abc" } }]}>
+            <BookStore/></Router>);
+
+    await waitFor(() => {
+            books.forEach((book) => {expect(screen.getByText(book.title)).toBeInTheDocument();});
+    });
+
+    const select = screen.getByTestId('quantity-picker-1');
+    fireEvent.change(select, { target: { value: '3' } });
+    fireEvent.click(screen.getByTestId('add-to-cart-1'));
+
+    await waitFor(() => {
+        books.forEach((book) => {expect(select.value).toBe('3');});
+        });
+     const cartValue = screen.getByTestId('cart-button');
+     expect(cartValue).toBeInTheDocument();
+     expect(cartValue).toHaveTextContent('Cart (3)');;
+  });
+
+
